8d1bcc5fb873f05a1cd51a7e240c43394f2ae391
elisedc <dscheng96@gmail.com>
2017-03-23 00:56:52 +0800

    merge

--- app/src/main/java/com/example/yang/flashtable/MainActivity.java
+++ app/src/main/java/com/example/yang/flashtable/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.yang.flashtable;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        dialogEvent();
     }
 
     private void dialogEvent() {
