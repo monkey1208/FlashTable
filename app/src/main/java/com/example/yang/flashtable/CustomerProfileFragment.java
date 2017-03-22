e3db57ee96005b5302382185d35144b4de96b638
elisedc <dscheng96@gmail.com>
2017-03-23 01:16:35 +0800

    fix

---
+++ app/src/main/java/com/example/yang/flashtable/CustomerProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.yang.flashtable;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CS on 2017/3/23.
+ */
+
+public class CustomerProfileFragment extends Fragment {
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.customer_main_fragment, container, false);
+
+        return view;
+    }
+}
