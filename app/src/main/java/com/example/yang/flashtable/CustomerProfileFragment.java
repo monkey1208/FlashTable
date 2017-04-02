90324c730dd381eaa5dbaa426145e40618626d58
elisedc <dscheng96@gmail.com>
2017-04-02 17:25:04 +0800

    profile credits

--- app/src/main/java/com/example/yang/flashtable/CustomerProfileFragment.java
+++ app/src/main/java/com/example/yang/flashtable/CustomerProfileFragment.java
@@ -1,32 +1,51 @@
 package com.example.yang.flashtable;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by CS on 2017/3/23.
  */
 
 public class CustomerProfileFragment extends Fragment {
+    SharedPreferences user;
+    String userID, username;
+
+    DialogBuilder dialog_builder;
 
     private View view;
+    TextView tv_username, tv_credit;
     LinearLayout ll_comments, ll_reservations;
     ImageView iv_avatar;
     Button bt_edit, bt_about_credits;
@@ -41,6 +60,10 @@ public class CustomerProfileFragment extends Fragment {
     }
 
     private void initView() {
+        dialog_builder = new DialogBuilder(getActivity());
+
+        tv_username = (TextView) view.findViewById(R.id.customer_profile_tv_name);
+        tv_credit = (TextView) view.findViewById(R.id.customer_profile_tv_credit);
         ll_reservations = (LinearLayout) view.findViewById(R.id.customer_profile_ll_reservations);
         ll_comments = (LinearLayout)  view.findViewById(R.id.customer_profile_ll_comments);
         iv_avatar = (ImageView) view.findViewById(R.id.customer_profile_iv_avatar);
@@ -49,6 +72,11 @@ public class CustomerProfileFragment extends Fragment {
     }
 
     private void initData() {
+        getUserInfo();
+
+        tv_username.setText(username);
+        new CustomerAPICredits().execute(userID);
+
         ll_reservations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,9 +99,62 @@ public class CustomerProfileFragment extends Fragment {
         bt_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
             }
         });
     }
 
+    private void getUserInfo() {
+        user = this.getActivity().getSharedPreferences("USER", MODE_PRIVATE);
+        userID = user.getString("userID", "");
+        username = user.getString("account", "");
+    }
+
+    class CustomerAPICredits extends AsyncTask<String, Void, String> {
+        private ProgressDialog progress_dialog = new ProgressDialog(CustomerProfileFragment.this.getActivity());
+        private String status = null;
+
+        @Override
+        protected void onPreExecute() {
+            // TODO: Style this.
+
+            progress_dialog.setMessage(getResources().getString(R.string.login_wait));
+            // progress_dialog.setContentView(R.layout.progress_dialog);
+            progress_dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String content = null;
+            HttpClient httpClient = new DefaultHttpClient();
+            try {
+                HttpGet request = new HttpGet(
+                        "https://flash-table.herokuapp.com/api/user_info?user_id=" + params[0]);
+                request.addHeader("Content-Type", "application/json");
+                HttpResponse response = httpClient.execute(request);
+                ResponseHandler<String> handler = new BasicResponseHandler();
+                String httpResponse = handler.handleResponse(response);
+                JSONObject responseJSON = new JSONObject(httpResponse);
+                status = responseJSON.getString("status_code");
+                if (status.equals("0")) {
+                    content = responseJSON.getString("point");
+                }
+            } catch (Exception e) {
+                Log.d("GetCode", "Request exception:" + e.getMessage());
+            } finally {
+                httpClient.getConnectionManager().shutdown();
+            }
+            return content;
+        }
+        @Override
+        protected void onPostExecute(String _content) {
+
+            if(status == null || status.equals("-1") || _content == null)
+                dialog_builder.dialogEvent(getResources().getString(R.string.login_error_connection), "normal", null);
+            else {
+                tv_credit.append(_content);
+            }
+            if (progress_dialog.isShowing())
+                progress_dialog.dismiss();
+        }
+    }
 }
