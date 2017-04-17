d185287a94ec7230e0f7cf1a7534d5b07cd288f8
elisedc <dscheng96@gmail.com>
2017-04-17 08:48:30 +0800

    search

--- app/src/main/java/com/example/yang/flashtable/CustomerSearchActivity.java
+++ app/src/main/java/com/example/yang/flashtable/CustomerSearchActivity.java
@@ -1,9 +1,12 @@
 package com.example.yang.flashtable;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by CS on 2017/4/15.
@@ -21,9 +24,11 @@ public class CustomerSearchActivity extends AppCompatActivity {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private void setupActionBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // Show the Up button in the action bar.
-            getSupportActionBar().setCustomView(R.layout.customer_search_bar);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            // Show the Up button in the action bar.View view = getLayoutInflater().inflate(R.layout.actionbar_customized_home, null);
+            //ActionBar.LayoutParams layout = new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.FILL_PARENT);
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setCustomView(R.layout.customer_search_bar);
+            // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
 }
