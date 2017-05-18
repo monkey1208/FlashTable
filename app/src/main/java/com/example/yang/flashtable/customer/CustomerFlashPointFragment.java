21428ec8369328e62139fc74dd3566ba603707a3
monkey1208 <b03902101@ntu.edu.tw>
2017-05-18 22:39:31 +0800

    Point fragment

---
+++ app/src/main/java/com/example/yang/flashtable/customer/CustomerFlashPointFragment.java
@@ -0,0 +1,26 @@
+package com.example.yang.flashtable.customer;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yang.flashtable.R;
+
+public class CustomerFlashPointFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.customer_flash_point_fragment, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+
+}
