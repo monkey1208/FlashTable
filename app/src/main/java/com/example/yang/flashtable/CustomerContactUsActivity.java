b7e9f03a9bd8301f1404aa7b1857cfc922e87f54
elisedc <dscheng96@gmail.com>
2017-05-29 23:37:58 +0800

    back

--- app/src/main/java/com/example/yang/flashtable/CustomerContactUsActivity.java
+++ app/src/main/java/com/example/yang/flashtable/CustomerContactUsActivity.java
@@ -2,8 +2,11 @@ package com.example.yang.flashtable;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by CS on 2017/5/29.
@@ -19,6 +22,14 @@ public class CustomerContactUsActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.customer_contact_activity);
+
+        ImageView bt_back = (ImageView) findViewById(R.id.customer_contact_bt_back);
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 
