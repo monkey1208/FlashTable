821519047f36b53b4a8ae3233961b176edbe0fff
elisedc <dscheng96@gmail.com>
2017-07-06 16:54:39 +0800

    add selectables

--- app/src/main/java/com/example/yang/flashtable/customer/CustomerShopActivity.java
+++ app/src/main/java/com/example/yang/flashtable/customer/CustomerShopActivity.java
@@ -29,7 +29,7 @@ import java.util.HashMap;
 public class CustomerShopActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
     private ShowInfo info;
     private String shop_id;
-    TextView tv_show_name, tv_show_consumption, tv_show_offer, tv_show_location, tv_show_category, tv_show_intro;
+    TextView tv_show_name, tv_show_consumption, tv_show_low, tv_show_offer, tv_show_location, tv_show_category;
     SliderLayout sl_restaurant;
     Button bt_show_reserve;
     RatingBar rb_show_rating;
@@ -68,9 +68,10 @@ public class CustomerShopActivity extends AppCompatActivity implements BaseSlide
         tv_show_name = (TextView) findViewById(R.id.customer_main_tv_show_shop);
         tv_show_offer = (TextView) findViewById(R.id.customer_main_tv_show_gift);
         tv_show_consumption = (TextView) findViewById(R.id.customer_main_tv_show_price);
+        tv_show_low = (TextView) findViewById(R.id.customer_main_tv_low);
         tv_show_location = (TextView) findViewById(R.id.customer_main_tv_show_location);
         tv_show_category = (TextView) findViewById(R.id.customer_main_tv_show_category);
-        tv_show_intro = (TextView) findViewById(R.id.customer_main_tv_show_description);
+        // tv_show_intro = (TextView) findViewById(R.id.customer_main_tv_show_description);
 
         ll_comments = (LinearLayout) findViewById(R.id.customer_main_ll_show_comments);
 
@@ -87,7 +88,6 @@ public class CustomerShopActivity extends AppCompatActivity implements BaseSlide
 
         shop_id = getIntent().getStringExtra("shop_id");
 
-        setDetail();
         ll_comments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,14 +97,12 @@ public class CustomerShopActivity extends AppCompatActivity implements BaseSlide
                 startActivity(intent);
             }
         });
+
+        setDetail();
     }
 
     private void setDetail(){
         sl_restaurant.removeAllSliders();
-        /*HashMap<String, Integer> image_map = new HashMap<>();
-        image_map.put("1", R.drawable.slide_1);
-        image_map.put("2", R.drawable.slide_2);
-        image_map.put("3", R.drawable.slide_3);*/
         HashMap<String, Bitmap> img_map = new HashMap<>();
         SqlHandler sqlHandler = new SqlHandler(this);
         ArrayList<Bitmap> list = sqlHandler.getBitmapList(Integer.parseInt(shop_id));
@@ -114,17 +112,7 @@ public class CustomerShopActivity extends AppCompatActivity implements BaseSlide
             index++;
         }
         for (String name : img_map.keySet()) {
-            // Change DefaultSliderView to TextSliderView if you want text below it
-            //DefaultSliderView slider_view = new DefaultSliderView(getBaseContext());
             BitmapSliderView slider_view = new BitmapSliderView(this);
-//            com.example.yang.flashtable.customer.slider.BaseSliderView slider_view =
-//                    new com.example.yang.flashtable.customer.slider.BaseSliderView(this) {
-//                        @Override
-//                        public View getView() {
-//                            return null;
-//                        }
-//                    };
-
             slider_view
                     .image(img_map.get(name))
                     .description(name)
@@ -141,10 +129,13 @@ public class CustomerShopActivity extends AppCompatActivity implements BaseSlide
         tv_show_name.setText(info.name);
         tv_show_consumption.setText("均消" + info.consumption + "元");
 
+        String low = info.low_consumption + getResources().getString(R.string.dollars);
+        tv_show_low.setText(low);
+
         tv_show_offer.setText(info.offer);
         tv_show_location.setText(info.address);
         tv_show_category.setText(info.category);
-        tv_show_intro.setText(info.intro);
+//        tv_show_intro.setText(info.intro);
         rb_show_rating.setRating(info.rating);
         rb_show_rating.setIsIndicator(true);
         bt_show_reserve.setOnClickListener(new View.OnClickListener() {
